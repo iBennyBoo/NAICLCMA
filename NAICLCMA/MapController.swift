feae584097af825554e0b87b609a7820378c7a9f
Ben Berman <bpb062004@gmail.com>
2022-01-11 07:28:27 -0600

    Have navigation controller and can swap between first and second floor

---
+++ NAICLCMA/MapController.swift
@@ -0,0 +1,32 @@
+//
+//  MapController.swift
+//  NAICLCMA
+//
+//  Created by Ben Berman on 1/11/22.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    @IBOutlet weak var map: UIImageView!
+    
+    var x = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchButton(_ sender: UIButton) {
+        if(x == 0){
+            x = 1
+            map.image = UIImage(named: "First Floor")
+        } else if (x == 1){
+            x = 0
+            map.image = UIImage(named: "Second Floor")
+        }
+    }
+    
+}
