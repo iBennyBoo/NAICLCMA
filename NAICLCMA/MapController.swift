21e64f7e7c46e9d52ff37a70405db4689bf7d961
CarlS <carl.show@yahoo.com>
2022-01-19 07:56:55 -0600

    Push so no explosions :]

--- NAICLCMA/MapController.swift
+++ NAICLCMA/MapController.swift
@@ -4,7 +4,6 @@ class MapController: UIViewController {
 
     @IBOutlet weak var map: UIImageView!
     @IBOutlet weak var switchButton: UIButton!
-    @IBOutlet weak var mapView: UIView!
     @IBOutlet var tapRecognizer: UITapGestureRecognizer!
     
     var x = 0
@@ -28,10 +27,11 @@ class MapController: UIViewController {
     
     @IBAction func onTap(_ sender: Any)
     {
-        if mapView.bounds.contains(tapRecognizer.location(in: mapView))
+        print(view.bounds.maxX)
+        if view.bounds.contains(tapRecognizer.location(in: view))
         {
-            print("\(tapRecognizer.location(in: mapView))")
+            print("\(tapRecognizer.location(in: view))")
+            print("\(((tapRecognizer.location(in: view).x)/view.bounds.maxX)*100), \(((tapRecognizer.location(in: view).y)/view.bounds.maxY)*100)")
         }
     }
-    
 }
