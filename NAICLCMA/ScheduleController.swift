5106564a9bc6f384f3ba4bbbbf0dc68dc669ce43
Ben Berman <bpb062004@gmail.com>
2022-01-20 08:08:29 -0600

    working on pickerView to tableView

--- NAICLCMA/ScheduleController.swift
+++ NAICLCMA/ScheduleController.swift
@@ -6,6 +6,9 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var pickerView: UIPickerView!
     
+    var selected: String!
+    var has: [String] = []
+    var count = 0
     var rooms: [String] = ["working", "106", "108", "109", "120", "122", "123", "124", "127", "128", "129", "130", "131", "132", "133", "134", "150", "151", "152", "153", "154", "155", "156", "158", "159", "160", "162", "163", "164", "165", "166", "167", "168", "169", "171", "172", "173", "200", "201", "201A", "202", "203", "204", "205", "205A", "206", "207", "208", "209", "209A", "210", "211", "212", "213", "214", "215", "216", "217", "218", "220", "221", "222", "223", "224", "225", "226", "230", "231", "232", "234", "235", "250", "251", "252", "253", "254", "255", "256", "259", "260", "261", "263", "264", "265", "266", "267", "268", "269", "AUD", "Athletic Director", "Band Room", "CAFE", "Care Room", "Choir Room", "Dean's Office", "FH", "Front Office", "GYM", "Guidance Office", "Internional Studies Office", "Library", "Nurse's Office", "Principal/Vice Office", "SRO Office", "Social Worker", "Student Services", "Tiger Conference Room", "WR"]
     
     override func viewDidLoad() {
@@ -24,6 +27,12 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
         
     }
     
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        has.append(selected)
+        tableView.reloadData()
+        count += 1
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -36,12 +45,18 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
         return rooms[row]
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //var place = pickerView.selectedRow(inComponent: row)
+        //selected = rooms[place]
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 9
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
         return cell
 }
     
