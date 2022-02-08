fb93ddede70ecea37d77801381265bcb5d6f1040
Ben Berman <bpb062004@gmail.com>
2022-02-08 08:16:42 -0600

    Very rudimentary lunch picker view finished

--- NAICLCMA/LunchController.swift
+++ NAICLCMA/LunchController.swift
@@ -1,16 +1,23 @@
 import UIKit
-
+import AVFoundation
 class LunchController: UIViewController {
 
     @IBOutlet weak var pickButton: UIButton!
+    @IBOutlet weak var border: UIImageView!
     @IBOutlet weak var pickLabel: UILabel!
     
+    var previous = 20
     let options: [String] = ["McDonalds", "Popeyes", "Raising Canes", "Chick-fil-a", "Culvers", "Panda Express", "Noodles & Co.", "Taco Bell", "Jimmy Johns", "Five Guys", "Wendys"]
+    
+    var audioPlayer: AVAudioPlayer?
+    let click = Bundle.main.path(forResource: "click", ofType: "mp3")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickButton.layer.borderWidth = 1
         pickLabel.layer.borderWidth = 1
+        border.layer.borderWidth = 1
+        pickLabel.isHidden = true
         
         let x = CAGradientLayer()
         x.frame = self.view.bounds
@@ -19,9 +26,24 @@ class LunchController: UIViewController {
     }
     
     @IBAction func pickButton(_ sender: UIButton) {
-        let random = Int.random(in: 0..<13)
+        
+        let url = URL(fileURLWithPath: click)
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        }catch{
+            print("error!")
+        }
+        
+        pickLabel.isHidden = false
+        let random = Int.random(in: 0..<11)
+        if(random != previous){
         print(random)
         pickLabel.text = "\(options[random])"
+        previous = random
+        } else{
+            pickLabel.text = "School Lunch"
+        }
     }
     
 }
