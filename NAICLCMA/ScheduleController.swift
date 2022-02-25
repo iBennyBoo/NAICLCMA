0ce111785076a3fcb50d8beffebd071b90709830
Ben Berman <bpb062004@gmail.com>
2022-02-25 07:23:35 -0600

    final adjustments

--- NAICLCMA/ScheduleController.swift
+++ NAICLCMA/ScheduleController.swift
@@ -16,7 +16,7 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
     var selectClass: Int = 1
     var has: [String] = ["---", "---", "---", "---", "---", "---", "---", "---", "---"]
     var periods: [Int] = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9]
-    var rooms: [String] = ["---", "106", "108", "109", "120", "122", "123", "124", "127", "128", "129", "130", "131", "132", "133", "134", "150", "151", "152", "153", "154", "155", "156", "158", "159", "160", "162", "163", "164", "165", "166", "167", "168", "169", "171", "172", "173", "200", "201", "201A", "202", "203", "204", "205", "205A", "206", "207", "208", "209", "209A", "210", "211", "212", "213", "214", "215", "216", "217", "218", "220", "221", "222", "223", "224", "225", "226", "230 // Band Room", "231", "232", "234 // Choir Room", "235", "250", "251", "252", "253", "254", "255", "256", "259", "260", "261", "263", "264", "265", "266", "267", "268", "269 // Library", "AUD", "Athletic Director", "CAFE", "Care Room", "Dean's Office", "FH", "Front Office", "GYM", "Nurse", "P. Offices", "SRO", "Social Worker", "Student Services", "Tiger Conference Room", "WR"]
+    var rooms: [String] = ["106", "108", "109", "120", "122", "123", "124", "127", "128", "129", "130", "131", "132", "133", "134", "150", "151", "152", "153", "154", "155", "156", "158", "159", "160", "162", "163", "164", "165", "166", "167", "168", "169", "171", "172", "173", "200", "201", "201A", "202", "203", "204", "205", "205A", "206", "207", "208", "209", "209A", "210", "211", "212", "213", "214", "215", "216", "217", "218", "220", "221", "222", "223", "224", "225", "226", "230 // Band Room", "231", "232", "234 // Choir Room", "235", "250", "251", "252", "253", "254", "255", "256", "259", "260", "261", "263", "264", "265", "266", "267", "268", "269 // Library", "AUD", "Athletic Director", "CAFE", "Care Room", "Dean's Office", "FH", "Front Office", "GYM", "Nurse", "P. Offices", "SRO", "Social Worker", "Student Services", "Tiger Conference Room", "WR"]
     
     let ding = Bundle.main.path(forResource: "Ding-small-bell", ofType: "mp3")!
     let bells = Bundle.main.path(forResource: "bells", ofType: "wav")!
@@ -39,7 +39,7 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
         
         button.layer.borderWidth = 1
         
-        if let items = UserDefaults.standard.data(forKey: "rooms6"){
+        if let items = UserDefaults.standard.data(forKey: "rooms"){
             let decoder = JSONDecoder()
             if let decoded = try? decoder.decode([String].self, from: items){
                 has = decoded
@@ -81,7 +81,7 @@ class ScheduleController: UIViewController, UITableViewDataSource, UITableViewDe
             }
             let encoder = JSONEncoder()
             if let encoded = try? encoder.encode(has){
-                UserDefaults.standard.set(encoded, forKey: "rooms6")
+                UserDefaults.standard.set(encoded, forKey: "rooms")
             }
             tableView.reloadData()
             if(has.count == 9){
