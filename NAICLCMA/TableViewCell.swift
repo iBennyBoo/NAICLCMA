38240d915824965e6282b2cac0dc931b7884b663
JayTucker2 <prosnipa1221@gmail.com>
2022-04-28 07:56:09 -0500

    TableView custom cell

---
+++ NAICLCMA/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  NAICLCMA
+//
+//  Created by JAYLAN TUCKER on 4/28/22.
+//
+
+import Foundation
+import UIKit
+class TableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+}
